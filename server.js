a664b93d2a1c7d6cc5a72d682fcb34e8270aedd7
Ihor Deyneka <ihor@univisia.com>
2025-10-24 18:02:21 +0300

    Fixed port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const crypto = require('crypto');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static('public'));
